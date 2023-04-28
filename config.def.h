54065f47915261dc95c71dfc5444e19b02dc594b
dissisor <serzh.gordienko.1997@gmail.com>
2023-04-28 19:06:03 +0300

    Changed some defults for my liking

--- config.def.h
+++ config.def.h
@@ -1,23 +1,26 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "JetBrains Mono:size=10", "Font Awesome 6 Brands:size=10", };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const char col_gray1[]         = "#222222";
+static const char col_gray2[]         = "#444444";
+static const char col_gray3[]         = "#bbbbbb";
+static const char col_gray4[]         = "#eeeeee";
+static const char col_black[]         = "#000000";
+static const char col_orange[]        = "#fcba03";
+static const char col_white1[]        = "#f5fffa";
+static const char col_white2[]        = "#f2f2f2";
+static const char col_cyan[]          = "#5de3f4";
+static const char *colors[][3]        = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_white2, col_gray1, col_black },
+	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan},
 };
-
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
