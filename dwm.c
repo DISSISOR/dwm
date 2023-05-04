358f8aeb730bd978747afd83830baa073a1951e4
dissisor <serzh.gordienko.1997@gmail.com>
2023-05-05 02:08:13 +0300

    fixed not showing current tags

--- dwm.c
+++ dwm.c
@@ -864,12 +864,13 @@ drawbar(Monitor *m)
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
 		w = TEXTW(tags[i]);
-                if (!(occ & 1 << i)) continue;
-		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
+                int sel = (m->tagset[m->seltags] & 1 << i);
+                if (!(sel || occ & 1 <<i)) continue;
+		drw_setscheme(drw, scheme[sel ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		if (occ & 1 << i)
 			drw_rect(drw, x + boxs, boxs, boxw, boxw,
-				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
+                                m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
 				urg & 1 << i);
 		x += w;
 	}
